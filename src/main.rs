75b4d0e635b8bd36a1aea3707aa9dee39a2a07b8
Jean Maccou <jmaccou@outlook.fr>
2024-02-17 23:28:34 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn next(board : [Vec<Vec<usize>>; 1], size : usize)->[Vec<Vec<usize>>; 1] {
+    count : u8 = 0;
+    for i in 0..size {
+        for j in 0..size {
+            
+        }
+    }
+}
+
+fn main() {
+    let size : usize = 5;
+    let mut board = [vec![vec![0; size]; size]];
+    board = next(board, size);
+}
